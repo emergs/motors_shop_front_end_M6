ee77a03f63db46042fe4f9a46570b0e557dc4a03
Fernando <fernandogynbsb@gmail.com>
2023-02-24 14:13:39 -0300

    fix: corrigindo o componento NoImageColor para receber props e modificando interfaces

--- src/components/NoImageColor/styles.ts
+++ src/components/NoImageColor/styles.ts
@@ -1,10 +1,20 @@
+import { HTMLAttributes, ReactNode } from "react";
 import styled from "styled-components";
 
 export const MyList = styled.li``;
 
-export const CommentImage = styled.div`
-    height: 35px;
-    width: 35px;
+export interface INoImageProps{
+    children: ReactNode;
+    width?: string;
+    height?: string;
+    color?: string;
+}
+
+
+
+export const CommentImage = styled.div<INoImageProps>`
+    height: ${(props) => props.height ? props.height : "35px"};
+    width: ${(props) => props.width ? props.width : "35px"};
     border-radius: 50%;
     background-color: ${(props) => props.color};
     display: flex;
