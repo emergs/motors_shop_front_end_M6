d08553b3218a1c553e5f137c89019d6e3a96689e
Fernando <fernandogynbsb@gmail.com>
2023-03-04 13:09:28 -0300

    fix: corrigindo conflitos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,21 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import LoginModal from "../components/Modais/LoginModal";
 import ProductsPage from "../components/ProductsPage";
-
-
 import Home from "../pages/Home";
-import Login from "../pages/OldLogin";
 import LoginUser from "../pages/LoginUser";
 import AdmPage from "../pages/ProfileViewAdm";
 import Register from "../pages/Register";
 
 const RoutesMain = () => {
-  return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/login2" element={<LoginUser />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/admview" element={<AdmPage />} />
-      <Route path="/product/:productId" element={<ProductsPage />} />
-      <Route path="*" element={<Navigate to="/home" />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<LoginUser />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admview" element={<AdmPage />} />
+            <Route path="/product/:productId" element={<ProductsPage />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+    );
 };
 
 export default RoutesMain;
