8121d7f39af8b488d6d029cab7a668e0a8acc615
Mazuquiebe Filho <97447610+Mazuquiebe@users.noreply.github.com>
2023-03-10 12:25:40 -0300

    Revert "Developer"

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import React from "react";
-import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
-import { GlobalStyle } from "./styles/global";
-import Providers from "./contexts";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Providers>
-        <GlobalStyle />
-        <App />
-      </Providers>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
